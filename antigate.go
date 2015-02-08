c365ec4c4b47e524714fe8ca7c727a807c2d37bd
Dmitry <poetinthecode@gmail.com>
2015-02-08 13:55:16 +0400

    Added captcha sample

--- antigate.go
+++ antigate.go
@@ -2,8 +2,27 @@ package antigate
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func ProcessCaptcha() {
-	fmt.Println("Process captcha: just implement this method =)")
+func ProcessCaptchaByUrl(url string) (string, error) {
+	fmt.Println(url)
+
+	// var bytes []byte
+	if _, err := loadImage(url); err != nil {
+		return "", err
+	}
+
+	return "", nil
+}
+
+func loadImage(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != 200 {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return nil, nil
 }
